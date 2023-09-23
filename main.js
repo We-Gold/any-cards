6412209989a5e988efec31319c1ff277519548e4
Weaver Goldman <wlgoldman@wpi.edu>
2023-09-23 09:01:30 -0400

    Add file upload and download support.

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { parseFileByDelimiter } from "./src/parse_by_delimiter"
 import { parsers } from "./src/standard_conversions"
 import "./style.css"
 
+import { saveAs } from 'file-saver'
+
 // Store references to the input and output textboxes
 const inputTextarea = document.querySelector("#input")
 const outputTextarea = document.querySelector("#output")
@@ -15,7 +17,7 @@ const outputTypeSelector = document.querySelector("#select-output")
 document.addEventListener("DOMContentLoaded", () => {
 	let parserOptions = ""
 
-	for (const parserKey of Object.keys(parsers)){
+	for (const parserKey of Object.keys(parsers)) {
 		parserOptions += `<option>${parserKey}</option>`
 	}
 
@@ -28,13 +30,22 @@ const runConversion = () => {
 	let converted = ""
 
 	// Determine which formats to convert between
-	const parser = parsers[inputTypeSelector.value]
-	const builder = parsers[outputTypeSelector.value]
+	const parser = getParser()
+	const builder = getBuilder()
 
 	try {
 		// Parse and convert the given file
-		const parsedFile = parseFileByDelimiter(inputTextarea.value, parser.delimiter, parser.newLineDelimiter)
-		converted = delimitParsedFile(parsedFile, builder.delimiter, builder.newLineDelimiter, [])
+		const parsedFile = parseFileByDelimiter(
+			inputTextarea.value,
+			parser.delimiter,
+			parser.newLineDelimiter
+		)
+		converted = delimitParsedFile(
+			parsedFile,
+			builder.delimiter,
+			builder.newLineDelimiter,
+			builder.columns
+		)
 	} catch (error) {
 		console.log(error)
 	}
@@ -42,7 +53,39 @@ const runConversion = () => {
 	outputTextarea.value = converted
 }
 
+const getParser = () => parsers[inputTypeSelector.value]
+const getBuilder = () => parsers[outputTypeSelector.value]
+
 // Run the conversion when any related values change
 inputTextarea.addEventListener("input", runConversion)
 inputTypeSelector.addEventListener("change", runConversion)
 outputTypeSelector.addEventListener("change", runConversion)
+
+// Handle file upload events
+document.querySelector("#input-upload").addEventListener(
+	"change",
+	(e) => {
+		const [file] = e.target.files
+
+		const reader = new FileReader()
+
+		reader.addEventListener(
+			"load",
+			() => {
+				inputTextarea.value = reader.result
+				runConversion()
+			},
+			false
+		)
+
+		if (file) reader.readAsText(file)
+	},
+	false
+)
+
+// Handle download requests
+document.querySelector("#download-file").addEventListener("click", () => {
+	const blob = new Blob([outputTextarea.value], {type: getBuilder().mimeType})
+
+	saveAs(blob)
+})
--- src/standard_conversions.js
+++ src/standard_conversions.js
@@ -1,9 +1,10 @@
-// TODO: Create custom option for choosing custom values
+// TODO: Create option for choosing custom columns
+// TODO: Create multi-types that only find matches between two strings
 
-const createParser = (delimiter, newLineDelimiter) => ({delimiter, newLineDelimiter})
+const createParser = (delimiter, newLineDelimiter, columns, mimeType) => ({delimiter, newLineDelimiter, columns, mimeType})
 
 export const parsers = {
-    "CSV": createParser(",", "\n"),
-    "OBSIDIAN-INLINE": createParser("::", "\n"),
-    "OBSIDIAN-MULTILINE": createParser("\n?\n", "\n\n"),
+    "CSV": createParser(",", "\n", [], "text/csv"),
+    "OBSIDIAN-INLINE": createParser("::", "\n", [0, 1], "text/markdown"),
+    "OBSIDIAN-MULTILINE": createParser("\n?\n", "\n\n", [0, 1], "text/markdown"),
 }
